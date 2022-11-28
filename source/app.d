2db313e4e00dba39e4cfb9d266c297ecb0791df9
Kirill Saidov <kirillsaidovd@yahoo.com>
2022-11-28 22:37:07 +0600

    first commit

---
+++ source/app.d
@@ -0,0 +1,78 @@
+module app;
+
+void main() {
+    import std.stdio: writeln;
+    import libloader;
+    import bindbc.glfw;
+    import bindbc.opengl;
+
+    // window constants
+    enum wWidth = 720;
+    enum wHeight = 420;
+    enum wTitle = "D/GLFW/OpenGL project";
+
+    // --- 1. load GLFW library
+    if(!load_glfw()) {
+        writeln("Failed to load GLFW library!");
+        return;
+    }
+
+    // --- 2. initialize GLFW
+    if(!glfwInit()) {
+        writeln("Failed to initialize GLFW library!");
+        return;
+    }
+    scope(exit) { glfwTerminate(); }
+
+    // --- 3. set default window hints and create an OpenGL context
+    glfwWindowHint(GLFW_SAMPLES, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    version(OSX) {
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, true);
+    }
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // create an actual window and attach current context
+    auto windowHandle = glfwCreateWindow(wWidth, wHeight, wTitle, null, null);
+    if(windowHandle is null) {
+        writeln("Failed to create a GLFW window!");
+        return;
+    }
+    scope(exit) { glfwDestroyWindow(windowHandle); windowHandle = null; }
+
+    // attach current context to GLFW window
+    glfwMakeContextCurrent(windowHandle);
+
+    // --- 3. load OpenGL library
+    if(!load_opengl()) {        
+        writeln("Failed to load OpenGL library!");
+        return;
+    }
+
+    // glViewport(0, 0, wWidth, wHeight);
+    glfwSetInputMode(windowHandle, GLFW_STICKY_KEYS, true);
+
+    bool quit = false;
+    while(!quit) {
+        // PROCESS EVENTS
+        glfwPollEvents();
+        if(glfwGetKey(windowHandle, GLFW_KEY_Q) == GLFW_PRESS) {
+            quit = true;
+        }
+
+        // UPDATE
+        // ...
+
+        // RENDER
+        // glClearColor(0, 0, 0, 0);
+        // glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        // {
+        //     // draw
+        //     // ...
+        // }
+        // glfwSwapBuffers(windowHandle);
+    }
+}
+
+
---
+++ source/libloader.d
@@ -0,0 +1,78 @@
+module libloader;
+
+import bindbc.glfw: GLFWSupport, loadGLFW, glfwSupport;
+import bindbc.opengl: GLSupport, loadOpenGL;
+
+import std.stdio: writefln;
+
+/++
+    Loads a shared GLFW library
+
+    Returns:
+        `true` upon success, `false` otherwise
++/
+bool load_glfw() {   
+    // set default dll lookup path for Windows
+    version(Windows) { 
+        import bindbc.loader: setCustomLoaderSearchPath;
+        setCustomLoaderSearchPath("libs"); 
+    }
+
+    // attempt to load GLFW
+    auto ret = loadGLFW();
+    if(ret != glfwSupport) {   
+        writefln(
+            ret == GLFWSupport.noLibrary ? "No GLFW library found!" : 
+            ret == GLFWSupport.badLibrary ? "A newer version of GLFW is needed. Please, upgrade!" : 
+            "Unknown error! Could not load OpenGL library!"
+        );
+        return false;
+    }
+    writefln("GLFW successfully loaded, version: %s", ret);
+    
+    return true;
+}
+
+/++ 
+    Loads OpenGL library
+    
+    Returns:
+        `true` upon success, `false` otherwise
++/
+bool load_opengl() {
+    auto ret = loadOpenGL();
+
+    // error checking
+    switch(ret) with(GLSupport) {
+        case gl46:
+        case gl45:
+        case gl44:
+        case gl43:
+        case gl42:
+        case gl41:
+        case gl40:
+        case gl33:
+        case gl32:
+        case gl31:
+        case gl30:
+            writefln("OpenGL successfully loaded, version: %s", ret);
+            return true;
+        case badLibrary:
+            writefln("The version of the GLFW library on your system is too low. Please upgrade.");
+            break;
+        case noContext:
+            writefln("Create an OpenGL context before attempting to load OpenGL!");
+            break;
+        case noLibrary:
+            writefln("OpenGL library not found!");
+            break;
+        default:
+            writefln("Unknown error! Could not load OpenGL library! Error code: %s", ret);
+            break;
+    }
+
+    return false;
+}
+
+
+
